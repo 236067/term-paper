58f3dfab6df62276fa197633695a1846fb3825b8
zhuxinglin <96447102+236067@users.noreply.github.com>
2022-11-22 01:57:36 +0300

    Update main.go

--- BikeWeb/main.go
+++ BikeWeb/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
-	"BikeWeb/handler"
-	"github.com/julienschmidt/httprouter"
+	"IhomeWeb/handler"
+	_ "IhomeWeb/model"
+	"github.com/go-acme/lego/log"
 	"go-micro/web"
-	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	//搭建web服务
+	// 构造web服务
 	service := web.NewService(
-		web.Name("go.micro.web.BikeWeb"),
+		web.Name("go.micro.web.IhomeWeb"),
 		web.Version("latest"),
-		web.Address(":10086"),
+		web.Address(":22333"),
 	)
-	//服务初始化
-	err := service.Init()
-	if err != nil {
+	// 服务初始化
+	if err := service.Init(); err != nil {
 		log.Fatal(err)
 	}
-	//路由
+	//构建路由
 	rou := httprouter.New()
+
+	//将路由注册到服务
 	rou.NotFound = http.FileServer(http.Dir("html"))
-	service.Handle("/", rou)
 	rou.GET("/api/v1.0/areas", handler.GetArea)
+	//欺骗浏览器  session index
+	rou.GET("/api/v1.0/session", handler.GetSession)
+	//session
 	rou.GET("/api/v1.0/house/index", handler.GetIndex)
+	//获取图片验证码
 	rou.GET("/api/v1.0/imagecode/:uuid", handler.GetImageCd)
-	rou.GET("/api/v1.0/users", handler.PostRet)
-	rou.GET("api/v1.0/sessions", handler.PostLogin)
-	rou.GET("/api/v1.0/session", handler.DeleteSession)
+	//用户注册
+	rou.POST("/api/v1.0/users", handler.PostRet)
+	//用户登陆
+	rou.POST("/api/v1.0/sessions", handler.PostLogin)
+	//退出登陆
+	rou.DELETE("/api/v1.0/session", handler.DeleteSession)
+	//获取用户详细信息
+	rou.GET("/api/v1.0/user", handler.GetUserInfo)
+	//用户上传图片
+	rou.POST("/api/v1.0/user/avatar", handler.PostAvatar)
+	//身份认证检查 同  获取用户信息   所调用的服务是 GetUserInfo
+	rou.GET("/api/v1.0/user/auth", handler.GetUserAuth)
 
-	//服务运行
-	err1 := service.Run()
-	if err1 != nil {
-		log.Fatal(err1)
+	service.Handle("/", rou)
+	// 服务运行
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
 	}
-
 }
