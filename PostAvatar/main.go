ec580c6e86f808ce7125132d3fa237f58f5a9d91
zhuxinglin <96447102+236067@users.noreply.github.com>
2022-11-22 02:00:25 +0300

    Add files via upload

---
+++ PostAvatar/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"PostAvatar/handler"
+	"github.com/micro/go-micro"
+	"log"
+
+	example "PostAvatar/proto/example"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.PostAvatar"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
