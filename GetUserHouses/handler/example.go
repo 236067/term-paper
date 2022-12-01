602132252d54437f1f0ed8bae90fd50f22e56166
zhuxinglin <96447102+236067@users.noreply.github.com>
2022-12-02 00:47:05 +0300

    Add files via upload

---
+++ GetUserHouses/handler/example.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	example "GetUserHouses/proto/example"
+	"IhomeWeb/model"
+	"IhomeWeb/utils"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego/adapter/orm"
+	"github.com/garyburd/redigo/redis"
+	"log"
+)
+
+type Example struct{}
+
+func (e *Example) GetUserHouses(ctx context.Context, req *example.Request, rsp *example.Response) error {
+	fmt.Println("获取当前用户所发布的房源 GetUserHouses /api/v1.0/user/houses")
+	rsp.Errno = utils.RECODE_OK
+	rsp.Errmsg = utils.RecodeText(rsp.Errno)
+	//连接redis
+	bm, err := utils.RedisOpen(utils.G_server_name, utils.G_redis_addr, utils.G_redis_port, utils.G_redis_dbnum)
+	if err != nil {
+		log.Println("redis connect err")
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+	}
+	value := bm.Get(req.Sessionid + "user_id")
+	user_id, _ := redis.Int(value, nil)
+	o := orm.NewOrm()
+	var houses []model.Bike
+	num, err := o.QueryTable("house").Filter("user__id", user_id).All(&houses)
+	if err != nil {
+		rsp.Errno = utils.RECODE_DBERR
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+	}
+	if num == 0 {
+		rsp.Errno = utils.RECODE_NODATA
+		rsp.Errmsg = utils.RecodeText(rsp.Errno)
+		return nil
+
+	}
+	//转换为二进制传输给前端
+	house, err := json.Marshal(houses)
+	rsp.Mix = house
+	return nil
+}
---
+++ GetUserHouses/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"GetUserHouses/handler"
+	example "GetUserHouses/proto/example"
+	"github.com/micro/go-micro"
+	"log"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.GetUserHouses"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
